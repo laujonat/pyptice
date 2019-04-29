7eddef0c8aff1ff21d7306c8593d9f6d6819c3fa
Jonathan Lau <jonhlau28@gmail.com>
2019-04-29 13:03:01 -0700

    Add stack to ll

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -74,6 +74,33 @@ class LinkedList(object):
                 prev = current
                 current = current.next
 
+    def insert_first(self, new_element):
+        "Insert new element as the head of the LinkedList"
+        new_element.next = self.head
+        self.head = new_element
+
+    def delete_first(self):
+        "Delete the first (head) element in the LinkedList as return it"
+        if not self.head:
+            return None
+        res = self.head
+        copy = res.next
+        self.head = copy
+        return res
+
+
+class Stack(object):
+    def __init__(self, top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        return self.ll.delete_first()
+
 
 # Test cases
 # Set up some Elements
@@ -107,3 +134,23 @@ print("delete pos 2, print 1", ll.get_position(1).value)
 print("pos 2, print 4", ll.get_position(2).value)
 # Should print 3 now
 print("pos 3, print 3", ll.get_position(3).value)
+
+# With Stack
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a Stack
+stack = Stack(e1)
+
+# Test stack functionality
+stack.push(e2)
+stack.push(e3)
+print(stack.pop().value)
+print(stack.pop().value)
+print(stack.pop().value)
+print(stack.pop())
+stack.push(e4)
+print(stack.pop().value)
