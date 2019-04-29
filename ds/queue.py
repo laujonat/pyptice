ac931f9909e6435e8427ddd4748de96731590ef1
Jonathan Lau <jonhlau28@gmail.com>
2019-04-29 13:27:34 -0700

    queue implementation

---
+++ ds/queue.py
@@ -0,0 +1,51 @@
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as
+few lines as possible.
+Make sure you pass the test cases too!"""
+
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        res = self.storage[0]
+        if len(self.storage) >= 1:
+            copy = []
+            for i in range(1, len(self.storage)):
+                copy.append(self.storage[i])
+            self.storage = copy
+        return res
+
+
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test peek
+# Should be 1
+print(q.peek())
+
+# Test dequeue
+# Should be 1
+print(q.dequeue())
+
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print(q.dequeue())
+# Should be 3
+print(q.dequeue())
+# Should be 4
+print(q.dequeue())
+q.enqueue(5)
+# Should be 5
+print(q.peek())
