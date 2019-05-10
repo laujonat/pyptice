68300cfa57e74d925cda61976d56117cccec181b
Jonathan Lau <jonhlau28@gmail.com>
2019-05-10 15:01:35 -0700

    k distinct substr

--- k_messed_array_sort.py
+++ k_messed_array_sort.py
@@ -4,6 +4,27 @@ function sortKMessedArray that sorts arr.
 
 For instance, for an input array of size 10 and k = 2, an element belonging to index 6 in the sorted array 
 will be located at either index 4, 5, 6, 7 or 8 in the input array.
+    
+n = arr.length
+
+# create an empty min-heap
+h = new MinHeap()
+
+# build the min-heap from the first k+1 elements of arr
+h.buildHeap(arr[0,...,k])
+
+for i from k+1 to n-1:
+    # extract the top element from the min-heap and
+    # assign it to the next available array index
+    arr[i-(k+1)] = h.extractMin()
+
+    # push the next array element into the min-heap
+    h.insert(arr[i])
+
+# extract all remaining elements from the min-heap
+# and assign them to the next available array index
+for i from 0 to k:
+    arr[n-k-1 + i] = h.extractMin()
 '''
 import heapq 
 
